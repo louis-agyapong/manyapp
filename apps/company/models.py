05156e441f2ffaef859b8b2a8bdc503e727be789
Louis Agyapong <agyapong.louis@gmail.com>
2021-12-05 03:41:50 +0000

    Company employee(Programmer model)

--- apps/company/models.py
+++ apps/company/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.fields import related
 from django.utils.translation import ugettext_lazy as _
 
 
@@ -11,3 +12,18 @@ class Company(models.Model):
     class Meta:
         verbose_name = _("Company")
         verbose_name_plural = _("Companies")
+
+
+class Programmer(models.Model):
+    """A company employee"""
+
+    name = models.CharField(_("Name"), max_length=100)
+    company = models.ForeignKey(
+        Company,
+        verbose_name=_("company"),
+        on_delete=models.CASCADE,
+        related_name="programmers",
+    )
+
+    def __str__(self):
+        return f"{self.name} works at {self.company}"
